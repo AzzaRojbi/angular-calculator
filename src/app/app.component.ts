9fc5e78db31973f9f34922fd6a49d21f20b1ab03
Azza Rojbi <arojbi@my.bcit.ca>
2017-11-18 19:45:43 -0800

    addition test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  public result:number;
+  public num1:string;
+  public num2:string;
+
+    addValues() {
+      this.result=Number(this.num1) + Number(this.num2);
+  console.log(this.result);
+    }
+  
 }
